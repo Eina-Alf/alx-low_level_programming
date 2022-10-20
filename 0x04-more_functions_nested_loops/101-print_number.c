17d0aadf7f8ec5940dde5956f39cd4ce6628d951
Eina-Alf <Eina-Alf>
2022-10-20 16:33:59 -0700

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,10 @@
  */
 
 void print_number(int n)
-
 {
 	unsigned int x;
 
 	x = n;
-
 	if (n < 0)
 	{
 		_putchar(45);
@@ -21,5 +19,4 @@ void print_number(int n)
 	if (x / 10)
 		print_number(x / 10);
 	_putchar((x % 10) + '0');
-
 }

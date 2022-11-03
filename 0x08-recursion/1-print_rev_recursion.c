fe062ebfd6b995dc4248e77d04ddadd07b433027
Eina-Alf <Eina-Alf>
2022-11-02 17:31:54 -0700

    tsk 2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,1 @@
-#include "main.h"
-/**
-* _print_rev_recursion - function that prints a string in reverse.
-*@s: parameter string
-*
-*/
-void _print_rev_recursion(char *s)
-{
-	if (*s != '\0')
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
-}
+
